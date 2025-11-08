617b4fd1f779ea29a2974f6a574abd9c951a8895
Aure6 <aurelien7112001@gmail.com>
2025-11-08 10:40:51 +0100

    `Refactor code to add multiple accounts and calculate total balance for a person`

--- SchmitAurelien/Program.cs
+++ SchmitAurelien/Program.cs
@@ -1,16 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var bank = new Bank("Our Bank");
-var owner = new Person("Alice", "Smith", new DateTime(1990, 5, 15));
-var account = new CurrentAccount("123456789", 500.0, 200.0, owner);
+var alice = new Person("Alice", "Smith", new DateTime(1990, 5, 15));
+var bob = new Person("Bob", "Jones", new DateTime(1985, 3, 22));
 
-account.Deposit(200.0);
-bool success = account.Withdraw(600.0); // Uses part of the credit line
+bank.AddAccount(new CurrentAccount("A1", 500.0, 200.0, alice));
+bank.AddAccount(new CurrentAccount("A2", 300.0, 150.0, alice));
+bank.AddAccount(new CurrentAccount("B1", 400.0, 100.0, bob));
 
-Console.WriteLine(account);
-Console.WriteLine("Withdrawal successful: " + success);
+// account1.Deposit(200.0);
+// bool success = account1.Withdraw(600.0); // Uses part of the credit line
+// Console.WriteLine(account1);
+// Console.WriteLine("Withdrawal successful: " + success);
+// Console.WriteLine(account1.GetBalance());
+// bank.AddAccount(account1);
+// bank.DeleteAccount("123456789");
 
-Console.WriteLine(account.GetBalance());
-
-bank.AddAccount(account);
-bank.DeleteAccount("123456789");
+double aliceTotal = bank.GetTotalBalanceForPerson(alice);
+Console.WriteLine($"Total balance for Alice: {aliceTotal:C}");
